ed90f5d26eef025535de1171034bb75619740eeb
Samir Fares <samirfares@MacBook-Pro-de-Samir.local>
2020-12-09 19:13:20 -0300

    start script + doc

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 
 
 // enable files upload
